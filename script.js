8eb86ce9581c42998a44f445c3a0bb60c0821e75
Patricia Stroka Ceballos <stroka.pati@gmail.com>
2025-01-20 15:37:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,15 +15,18 @@ document.getElementById("check-btn").addEventListener("click", () => {
   }
 });
 
-/* Code for clearing the results: */
+/* Code for clearing the results and input field: */
 document.getElementById("clear-btn").addEventListener("click", () => {
+  // Clear the input field
+  document.getElementById("user-input").value = "";
+
+  // Clear the results div
   const resultsDiv = document.getElementById("results-div");
   resultsDiv.innerHTML = ""; // Clears the content in the results div
 });
 
 /* Number Validator for phone number validation: */
 function numberValidator(str) {
-  
   let regExp = /^(1\s?)?(\(\d{3}\)|\d{3})[\-\s]?\d{3}[\-\s]?\d{4}$/;
   return regExp.test(str);
 }
